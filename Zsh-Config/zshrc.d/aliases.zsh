23fd03c08516ba9382984fceb9568b1189984d74
Kat404 <71116973+Kat404@users.noreply.github.com>
2025-09-29 14:23:49 -0600

    Mejor estructura de comentarios en el archivo 'aliases.zsh'

--- Zsh-Config/zshrc.d/aliases.zsh
+++ Zsh-Config/zshrc.d/aliases.zsh
@@ -2,16 +2,16 @@
 # ~/.zshrc.d/aliases.zsh
 
 # =============================================
-# 1. COMANDOS DEL SISTEMA
+# 1.          COMANDOS DEL SISTEMA
 # =============================================
 # Actualización del sistema y utilidades básicas
-alias update='sudo apt update && sudo apt upgrade -y && flatpak update' # <-- Cambia este comando según la distribución de Linux que utilices
+alias update='sudo apt update && sudo apt upgrade -y && flatpak update' 
 alias fsh='fastfetch' # <-- Menos letras = Más velocidad
 alias salir='exit' # <-- Españolización
 alias cls='clear' # <-- Menos letras = Más velocidad
 
 # =============================================
-# 2. NAVEGACIÓN DE DIRECTORIOS
+# 2.        NAVEGACIÓN DE DIRECTORIOS
 # =============================================
 # Comandos para moverse entre directorios
 alias ir='cd' # <-- Españolización
@@ -19,22 +19,21 @@ alias irq='cd ..' # <-- Subconjunto del comando anterior, se prioriza velocidad 
 alias cds='yazi' # <-- Mayor coherencia sobre la función de yazi, se prioriza velocidad de tecleo
 
 # =============================================
-# 3. COMANDOS LS
+# 3.           COMANDOS LS
 # =============================================
 # Se prioriza dar una experiencia out-of-the-box con una buena gama de comandos para 'ls', personaliza a tu gusto personal
 alias ll='ls -lh --color'
 alias l='ls -CFA --color'
 
 # =============================================
-# 4. HERRAMIENTAS Y ACCESOS DIRECTOS
+# 4.     HERRAMIENTAS Y ACCESOS DIRECTOS
 # =============================================
 # Accesos rápidos a herramientas externas
-# 'icat' solo funciona en la terminal Kitty, si usas otra terminal, descomenta la siguiente línea
-# alias icat='kitten icat' # <-- Shortcut para el kitten de icat el cual permite visualizar imágenes en la terminal
-# alias bloquear='i3lock-fancy -g -p' # <-- Descomenta si usas Desktop Locker, bloqueo de pantalla visualmente bonito
+alias icat='kitten icat'
+alias bloquear='i3lock-fancy -g -p' 
 
 # =============================================
-# 5. MANEJO DE ARCHIVOS
+# 5.          MANEJO DE ARCHIVOS
 # =============================================
 # Comandos para manipulación de archivos
 alias untar='tar -xf' # <-- Shortcut para el comando tar, siguiendo como ejemplo 'unzip' para archivos '.zip'
